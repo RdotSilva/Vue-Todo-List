cde240f6604b977c75f5621c63479f1e8bc3b40e
Ryan Silva <RyandotSilva@gmail.com>
2019-05-04 18:01:52 -0400

    Add style to Header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -10,4 +10,17 @@ export default {
 }
 </script>
 
+<style scoped>
+    .header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
 
+  .header a {
+    color: #fff;
+    padding-right: 5px;
+    text-decoration: none;
+  }
+</style>
