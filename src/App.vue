7db07e645c49a19552e86a1dd9a991f048ceb471
Ryan Silva <RyandotSilva@gmail.com>
2019-05-06 16:41:14 -0400

    Remove placeholder todo items

--- src/App.vue
+++ src/App.vue
@@ -21,21 +21,6 @@ export default {
   data() {
     return {
       todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
       ]
     }
   },
